811364343f4eef47be742e880c2227ac30256ef6
Hollin Wilkins <hollinrwilkins@gmail.com>
2019-12-07 11:08:08 -0800

    Use options parses for running different experiments.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,32 @@
 #[macro_use]
 extern crate slog;
 
+#[macro_use]
+extern crate clap;
+
 mod raft_multi;
+mod f2s_copy;
+
+use clap::App;
+
+#[derive(Clap)]
+#[clap(version = "v1.0-beta")]
+struct Opts {
+    main: String
+}
 
 fn main() {
-    raft_multi::run();
+    let opts = Opts::parse();
+
+    match opts.main.as_ref() {
+        "raft_multi" => {
+            raft_multi::run();
+        },
+        "f2s_copy" => {
+            f2s_copy::run();
+        },
+        main => {
+            println!("provided an invalid main {}", main);
+        }
+    };
 }
